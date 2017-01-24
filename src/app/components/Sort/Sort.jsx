b12b26dd74dd4d448a1f8042c634624d6db6ae15
Nedjim DANIMON NGABA <nedjim.danimon@gmail.com>
2017-01-24 11:30:52 +0100

    ADD: sort component

--- src/app/components/Sort/Sort.jsx
+++ src/app/components/Sort/Sort.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Popular from '../Dropdown/Popular.jsx';
+import ShotsSort from '../Dropdown/ShotsSort.jsx';
+import Now from '../Dropdown/Now.jsx';
 
 export default class Sort extends React.Component {
     render() {
         return (
             <div id="sort">
                 <Popular />
+                <ShotsSort />
+                <Now />
             </div>
         );
     }
