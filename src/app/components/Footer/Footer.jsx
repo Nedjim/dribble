da8243d7f7c1b1de8ecdda59ca99628ab202a79c
Nedjim DANIMON NGABA <nedjim.danimon@gmail.com>
2017-01-25 19:04:20 +0100

    ADD: footer

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -11,6 +11,19 @@ export default class Footer extends React.Component {
                 <List />
                 <Jobs />
                 <p id="copyright">Copyright © 2009–2017 Dribbble LLC. All screenshots © their respective owners. Shipped from Salem, Mass. USA.</p>
+                <div id="social-network" className="inline-block">
+                    <img src="../../img/dribbble-icon.png" alt="dribble"/>
+                    <img src="../../img/twitter.png" alt="twitter"/>
+                    <img src="../../img/fbk.png" alt="facebook"/>
+                    <img src="../../img/instagram.png" alt="instagram"/>
+                </div>
+                <div id="contact" className="inline-block absolute">
+                    <img src="../../img/dribbble-ball.png" className="inline-block"/>
+                    <div className="inline-block absolute">
+                        <p>662,298,206,839</p>
+                        <p>pixels dribbbled</p>
+                    </div>
+                </div>
             </footer>
         );
     }
